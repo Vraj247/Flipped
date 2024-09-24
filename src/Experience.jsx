8e4a0c5c3f71d662070bffe38df4422be87302ae
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2024-09-24 13:48:37 +0530

    Context

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,64 +1,52 @@
-import React, { Suspense } from 'react';
-import { Text, Html, ContactShadows, PresentationControls, Float, Environment, useGLTF } from '@react-three/drei'
+import React, { useEffect, useRef } from 'react';
+import { Html, ContactShadows, PresentationControls, Float, Environment, useGLTF } from '@react-three/drei';
 
 export default function Experience() {
-
-    const computer = useGLTF('https://threejs-journey.com/resources/models/macbook_model.gltf')
-    console.log(computer)
-
-    return <>
-
-        <Environment preset="city" />
-        <color args={['#FFA500']} attach="background" />
-        <PresentationControls
-            global
-            rotation={[0, 0, 0]}
-            polar={[-0.4, 0.2]}
-            azimuth={[-1, 0.75]}
-        // config={{ mass: 10, tension: 500 }}
-        // snap={{ mass: 4, tension: 400 }}
-        >
-            <Float rotationIntensity={0.4}>
-                <primitive
-                    object={computer.scene}
-                    position-y={-1.3}
-                >
-                    {/* <rectAreaLight
-                        width={2.5}
-                        height={1.4}
-                        intensity={20}
-                        color={'#fa4f01'}
-                        rotation={[0.1, Math.PI, 0]}
-                        position={[0, 0.5, -1.5]}
-
-                    /> */}
-
-                    <Html
-                        transform
-                        wrapperClass='htmlScreen'
-                        distanceFactor={1.17}
-                        position={[0, 1.56, -1.4]}
-                        rotation-x={-0.256}
+    const computer = useGLTF('https://threejs-journey.com/resources/models/macbook_model.gltf');
+    const videoRef = useRef(null); // Reference to the video element
+  
+    return (
+        <>
+            <Environment preset="studio" background backgroundBlurriness={0.6} />
+            <color args={['#01010']} attach="background" />
+            <PresentationControls
+                global
+                rotation={[0, 0, 0]}
+                polar={[-0.0, 0.0]}
+                azimuth={[-1, 0.75]}
+                config={{ mass: 10, tension: 500 }}
+                snap={{ mass: 4, tension: 400 }}
+            >
+                <Float rotationIntensity={0.4}>
+                    <primitive
+                        object={computer.scene}
+                        position-y={-1.0}
                     >
-                        <iframe src="https://vry.works/"></iframe>
-                    </Html>
-                </primitive>
+                        <Html
+                            transform
+                            wrapperClass='htmlScreen'
+                            distanceFactor={1.17}
+                            position={[0, 1.56, -1.4]}
+                            rotation-x={-0.256}
+                        >
+                            <video
+                                ref={videoRef} // Attach ref to video element
+                                src="steve.mp4"
+                                autoPlay
+                                loop
 
-            </Float>
-        </PresentationControls>
-        {/* <Text
-            position={[0, 0, 0]}
-            rotation={[0, 0, 0]}
-            fontSize={10}
-            maxWidth={2}
-        >Hello World</Text> */}
-
-        <ContactShadows
-            position-y={-1.2}
-            opacity={0.3}
-            blur={0.3}
-            scale={13}
-        />
-
-    </>
+                                style={{ width: '100%', height: '100%', borderRadius: '0px', opacity:'0.8' }}
+                            ></video>
+                        </Html>
+                    </primitive>
+                </Float>
+            </PresentationControls>
+            <ContactShadows
+                position-y={-2.2}
+                opacity={0.2}
+                blur={1.2}
+                scale={24}
+            />
+        </>
+    );
 }
